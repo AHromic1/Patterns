f31bf385f454b088044666de5f53768b6bb7df78
AHromic1 <115954313+AHromic1@users.noreply.github.com>
2023-05-17 01:44:38 +0200

    Add files via upload
    Singleton pattern

---
+++ Singleton/ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+
+
+// Usage
+King kralj1;
+kralj1= King.getInstance();
+King kralj2;
+kralj2 = King.getInstance();
+
+if(kralj1==kralj2)
+
+Console.WriteLine("Postoji samo jedan kralj!");  // Da li su vracene 2 iste instance iz klase, odnosno da li postoji samo jedan kralj?
